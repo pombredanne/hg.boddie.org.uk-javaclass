192877d9f9c41a148a528cb9648cc08d80c0bfe2
Paul Boddie <paul@boddie.org.uk>
2004-11-09 19:49:04 +0100

    Added more test methods.

--- tests/Value.java
+++ tests/Value.java
@@ -12,6 +12,24 @@ public class Value {
     public void setValue(int value) {
         this.value = value;
     }
+
+    public boolean isPositive() {
+        return this.value > 0;
+    }
+
+    public int compare(int value) {
+        if (value < this.value) {
+            return -1;
+        } else if (value == this.value) {
+            return 0;
+        } else {
+            return 1;
+        }
+    }
+
+    public int add(int value) {
+        return this.value + value;
+    }
 }
 
 // vim: tabstop=4 expandtab shiftwidth=4
