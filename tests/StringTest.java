4e474612d66ced6523f2efbfaeee7a91b971e382
Paul Boddie <paul@boddie.org.uk>
2004-11-21 23:48:35 +0100

    Added a test of java.lang.String.

---
+++ tests/StringTest.java
@@ -0,0 +1,14 @@
+public class StringTest {
+    private String s;
+    private String s2 = new String("abc");
+    private static String s3;
+    private static String s4 = new String("xyz");
+
+    public StringTest() {
+        s = s2;
+    }
+
+    public StringTest(String newString) {
+        s = newString;
+    }
+}
