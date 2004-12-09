6e38191c64f84ec4c16624b2324f78a1b63cab23
Paul Boddie <paul@boddie.org.uk>
2004-12-09 02:18:13 +0100

    Added a simple comparison test.

---
+++ tests/ComparisonTest.java
@@ -0,0 +1,26 @@
+public class ComparisonTest {
+
+    public boolean equals(int x, int y) {
+        if (x == y) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public boolean lessThan(int x, int y) {
+        if (x < y) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public boolean greaterThan(int x, int y) {
+        if (x > y) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
