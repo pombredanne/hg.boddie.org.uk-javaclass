f794d00c261c7945a33a59518288d817636e7733
Paul Boddie <paul@boddie.org.uk>
2005-01-10 23:43:02 +0100

    Added button configuration and callbacks.

--- examples/Tkinter/Application.java
+++ examples/Tkinter/Application.java
@@ -1,5 +1,5 @@
 public class Application extends tkjava.Frame {
-    private tkjava.Button quit, hiThere;
+    private tkjava.Button QUIT, hiThere;
 
     public Application() {
         super();
@@ -7,10 +7,31 @@ public class Application extends tkjava.Frame {
         this.createWidgets();
     }
 
+    public void sayHello() {
+        System.out.println("hi there, everyone!");
+    }
+
     public void createWidgets() {
-        this.quit = new tkjava.Button(this);
-        this.quit.pack();
+
+        // Instead of just getting tkjava.Frame.quit or even this.quit...
+
+        Class[] empty = new Class[0];
+        java.lang.reflect.Method quit = null, sayHello = null;
+        try {
+            quit = this.getClass().getMethod("quit", empty);
+            sayHello = this.getClass().getMethod("sayHello", empty);
+        } catch (NoSuchMethodException exc) {
+            // Methods remain as null -> None and will cause runtime errors.
+        }
+
+        this.QUIT = new tkjava.Button(this);
+        this.QUIT.__setitem__("text", "QUIT");
+        this.QUIT.__setitem__("fg", "red");
+        this.QUIT.__setitem__("command", quit);
+        this.QUIT.pack();
         this.hiThere = new tkjava.Button(this);
+        this.hiThere.__setitem__("text", "Hello");
+        this.hiThere.__setitem__("command", sayHello);
         this.hiThere.pack();
     }
 }
