573f9dfa7b866533eb238779cc01ef4c76b896cb
Paul Boddie <paul@boddie.org.uk>
2005-01-18 17:47:24 +0100

    Added more test programs.

--- tests/ArrayTest.java
+++ tests/ArrayTest.java
@@ -8,4 +8,25 @@ public class ArrayTest {
     public int get(int index) {
         return array[index];
     }
+
+    public static void main(String[] args) {
+        ArrayTest test = new ArrayTest(10);
+        if (test.array.length != 10) {
+            System.err.println("test.array.length failed!");
+        } else {
+            System.out.println("test.array.length correct: " + test.array.length);
+        }
+        for (int i = 0; i < test.array.length; i++) {
+            test.array[i] = i + 10;
+        }
+        for (int j = 0; j < test.array.length; j++) {
+            if (test.get(j) != j + 10) {
+                System.err.println("test.get(" + j + ") failed!");
+            } else {
+                System.out.println("test.get(" + j + ") correct: " + test.get(j));
+            }
+        }
+    }
 }
+
+// vim: tabstop=4 expandtab shiftwidth=4
