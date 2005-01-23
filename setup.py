ae3239b8b78b3648978b064946600a2106b9a232
Paul Boddie <paul@boddie.org.uk>
2005-01-23 01:59:27 +0100

    Added release procedures to the documentation. Changed the acquisition of version information in setup.py.

--- setup.py
+++ setup.py
@@ -2,13 +2,15 @@
 
 from distutils.core import setup
 
+import javaclass
+
 setup(
     name         = "javaclass",
     description  = "A Java class and package importer with utilities.",
     author       = "Paul Boddie",
     author_email = "paul@boddie.org.uk",
     url          = "http://www.boddie.org.uk/python/javaclass.html",
-    version      = "0.1",
+    version      = javaclass.__version__,
     packages     = ["javaclass", "java", "java.lang", "java.security"],
     scripts      = ["runclass.py"]
     )
