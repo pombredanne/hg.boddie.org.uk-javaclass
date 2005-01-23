b6da4420a2c075778eb71241519ed00786d9d187
Paul Boddie <paul@boddie.org.uk>
2005-01-23 01:19:42 +0100

    Fixed the names and grammar.

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@
 from distutils.core import setup
 
 setup(
-    name         = "ClassFile",
-    description  = "A Java class and package importer and utilities.",
+    name         = "javaclass",
+    description  = "A Java class and package importer with utilities.",
     author       = "Paul Boddie",
     author_email = "paul@boddie.org.uk",
-    url          = "http://www.boddie.org.uk/python/ClassFile.html",
+    url          = "http://www.boddie.org.uk/python/javaclass.html",
     version      = "0.1",
     packages     = ["javaclass", "java", "java.lang", "java.security"],
     scripts      = ["runclass.py"]
